095ff09f7e34ac0b4a219297fe2f6c292e261711
tuanphan1998 <tuanphani.c.t@gmail.com>
2021-05-16 20:59:37 +0700

    ok change

--- src/Components/SectionStatesCity/index.js
+++ src/Components/SectionStatesCity/index.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import {Row} from 'reactstrap';
 import ItemStateCity from './Future1/itemStateCity';
 import {getData,getAqi} from '../../Api/GlobalApi';
+import ItemStateUv from './Future1/itemStateUv';
 
 function Statecity() {
     const dauInput = useSelector(state => state.search.value);
@@ -15,6 +16,7 @@ function Statecity() {
         <>
                 <Row>
                 <ItemStateCity/>
+                <ItemStateUv/>
                 </Row>
         </>
     )
