35b3b168e65dffd26969312ab2884bbd9c0613d2
tuanphan1998 <tuanphani.c.t@gmail.com>
2021-05-16 14:34:07 +0700

    ok change

--- src/Components/SectionStatesCity/Future1/itemStateCity.js
+++ src/Components/SectionStatesCity/Future1/itemStateCity.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Styled from 'styled-components';
 import {useSelector} from 'react-redux';
 import { Col } from 'reactstrap';
+import ItemStateUv from './itemStateUv';
 const ColStyle = Styled(Col) `
 padding-top: 25px;
 `;
@@ -20,18 +21,14 @@ const DescriptionHeading2 = Styled.h2 `
 color: white;
 font-size: 14px;
 `;
-const DescriptionHeading3 = Styled.h2 `
-color: white;
-font-size: 20px;
-`;
+
 const IconSun = Styled.svg `
 color: white;
 `;
 function ItemStateCity() {
     const ketqua2 = useSelector(state => state.GlobalApi.status);
     const ketqua = useSelector(state => state.GlobalApi.value);
-    const ketqua3 = useSelector(state => state.GlobalApi.status3);
-    const ketqua33 = useSelector(state => state.GlobalApi.value3);
+ 
     const renderCity = () => {
         if(String(ketqua.cod) === "200")
         {
@@ -57,66 +54,6 @@ function ItemStateCity() {
         d.setDate(d.getDate() + (1 + 7 - d.getDay()) % 7);
         return(''+d)
     }
-    const renderDateUvIndex = () => {
-        if(ketqua33.data==="Unknown station")
-        {
-            return('Unknown station');
-        }else{
-            return (ketqua33.data.forecast.daily.uvi[2].day)
-        }
-    }
-    const renderUvIndex = () => {
-        if(ketqua33.data==="Unknown station")
-        {
-            return('Unknown station');
-        }else{
-            switch (ketqua33.data.forecast.daily.uvi[2].max) {
-                case 1:
-                case 2:
-                    return(ketqua33.data.forecast.daily.uvi[2].max + " (Mức thấp)");
-                case 3:
-                case 4:
-                case 5:
-                    return(ketqua33.data.forecast.daily.uvi[2].max + " (Mức trung bình)");
-                case 6:
-                case 7:
-                    return(ketqua33.data.forecast.daily.uvi[2].max + " (Mức cao)");
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                    return(ketqua33.data.forecast.daily.uvi[2].max + " (Mức cực kỳ cao)");
-                default:
-                    return null;
-            }
-        }
-    }
-    const renderAws = () => {
-        if(ketqua33.data==="Unknown station")
-        {
-            return('Unknown station');
-        }else{
-            switch (ketqua33.data.forecast.daily.uvi[2].max) {
-                case 1:
-                case 2:
-                    return('Theo ô nhỏ');
-                case 3:
-                case 4:
-                case 5:
-                    return('Theo áo chống nắng, kính dâm, Kem chống nắng');
-                case 6:
-                case 7:
-                    return('Theo áo chống nắng, kính dâm, Kem chống nắng, Hạn chế tiếp xúc dưới nắng');
-                case 8:
-                case 9:
-                case 10:
-                case 11:
-                    return('Theo áo chống nắng, kính dâm, Kem chống nắng, Không nên ra ngoài lúc này');
-                default:
-                    return null;
-            }
-        }
-    }
     return (
         <>
                  <ColStyle xl="6">
@@ -129,14 +66,7 @@ function ItemStateCity() {
                         <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
                     </IconSun>
                 </ColStyle2>
-                <ColStyle2 xl="3">
-                    <DescriptionHeading3 className="display-6 display-6 d-flex justify-content-start mt-4">Tỉ số tia UV ngày {ketqua3 === "thanhcong" ? renderDateUvIndex() : "loading"}</DescriptionHeading3>
-                    <DescriptionHeading3 className="display-6 display-6 d-flex justify-content-start mt-1">UV: {ketqua3 === "thanhcong" ? renderUvIndex() : "loading"}</DescriptionHeading3>
-                    <DescriptionHeading3 className="display-6 display-6 d-flex justify-content-start mt-1">Hãy mang: {ketqua3 === "thanhcong" ? renderAws() : "loading"}</DescriptionHeading3>
-                </ColStyle2>
-                <ColStyle2 xl="1">
-                    <DescriptionHeading3 className="display-6 display-6 d-flex justify-content-start mt-4">Tỉ số tia UV ngày {ketqua3 === "thanhcong" ? renderDateUvIndex() : "loading"}</DescriptionHeading3>
-                </ColStyle2>
+                <ItemStateUv/>
         </>
     )
 }
