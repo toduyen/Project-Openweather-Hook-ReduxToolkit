f03923eac0ea84544458e00eff8d28e7fa690e15
tuanphan1998 <tuanphani.c.t@gmail.com>
2021-06-12 22:15:05 +0700

    change position icon sun

--- src/Components/SectionStatesCity/Views/itemStateCity.js
+++ src/Components/SectionStatesCity/Views/itemStateCity.js
@@ -7,6 +7,7 @@ padding-top: 25px;
 `;
 const ColStyle2 = Styled(Col) `
 padding-top: 6px;
+padding-right: 40px;
 `;
 const Heading = Styled.h2 `
 color: white;
@@ -60,7 +61,7 @@ function ItemStateCity() {
                     <DescriptionHeading className="display-6 d-flex justify-content-center">{ngaythangnam()}</DescriptionHeading>
                     <DescriptionHeading2 className="display-6 d-flex justify-content-center">Đã thiết lập tự động cập nhật 30 phút</DescriptionHeading2>
                 </ColStyle>
-                <ColStyle2 xl="2" sm="6" className="d-flex justify-content-center mt-5 col-6">
+                <ColStyle2 xl="2" sm="6" className="d-flex justify-content-end mt-5 col-6">
                     <IconSun xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="currentColor" className="bi bi-sun" viewBox="0 0 16 16">
                         <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
                     </IconSun>
