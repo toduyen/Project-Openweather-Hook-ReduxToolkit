e6fe7e08e5be1433190b8fb9143b7ce6b2f1ea3f
toduyen <toduyen0402@gmail.com>
2021-09-20 20:20:12 +0700

    ad

--- src/Components/SectionStatesCity/Views/itemStateUv.js
+++ src/Components/SectionStatesCity/Views/itemStateUv.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import Styled from 'styled-components';
 import { Col } from 'reactstrap';
+import Skeleton from 'react-loading-skeleton';
+import PropTypes from 'prop-types';
 const DescriptionHeading3 = Styled.h2`
 color: white;
 font-size: 20px;
@@ -20,7 +22,7 @@ function ItemStateUv() {
   const ketqua = useSelector(state => state?.GlobalApi?.value);
   const renderUvIndex = () => {
     if (ketqua33.data === "Unknown station") {
-      return ('');
+      return <Skeleton/>;
     } else {
       try {
         switch (ketqua33?.data?.forecast?.daily?.uvi[2]?.max) {
@@ -40,17 +42,17 @@ function ItemStateUv() {
           case 11:
             return ("UV "+ketqua33?.data?.forecast?.daily?.uvi[2]?.max);
           default:
-            return null;
+            return <Skeleton/>;
         }
       }
       catch (error) {
-        return ('');
+        return <Skeleton/>;
       }
     }
   }
   const renderAws = () => {
     if (ketqua33.data === "Unknown station") {
-      return null;
+      return <Skeleton/>;
     } else {
       try {
         switch (ketqua33?.data?.forecast?.daily?.uvi[2]?.max) {
@@ -70,24 +72,40 @@ function ItemStateUv() {
           case 11:
             return ('Hãy mang: áo chống nắng, kính dâm, Kem chống nắng, Không nên ra ngoài lúc này. Tia uv ở mức rất cao');
           default:
-            return null;
+            return <Skeleton/>;
         }
       }
       catch (error) {
-        return null;
+        return <Skeleton/>;
       }
     }
   }
   return (
     <>
       <ColStyle2 xl="4" sm="6" className="col-6">
-        {ketqua?.cod === "404" ? null  : <React.Fragment>
-          <Heading className="d-flex justify-content-start mt-4">{ketqua3 === "thanhcong" ? renderUvIndex() : "loading"}</Heading>
-          <DescriptionHeading3 className="display-6 display-6 d-flex justify-content-center mt-1">{ketqua3 === "thanhcong" ? renderAws() : "loading"}</DescriptionHeading3>
+        {ketqua?.cod === "404" ? <Skeleton/>  : <React.Fragment>
+          <Heading className="d-flex justify-content-start mt-4">{ketqua3 === "thanhcong" ? renderUvIndex() : <Skeleton/>}</Heading>
+          <DescriptionHeading3 className="display-6 display-6 d-flex justify-content-center mt-1">{ketqua3 === "thanhcong" ? renderAws() : <Skeleton/>}</DescriptionHeading3>
         </React.Fragment>}
       </ColStyle2>
     </>
   )
 }
-
+ItemStateUv.propTypes = {
+  ketqua33: PropTypes.shape({
+    data: PropTypes.string,
+    data : PropTypes.shape({
+      forecast: PropTypes.shape({
+        daily: PropTypes.shape({
+          uvi: PropTypes.shape({
+            max: PropTypes.string
+          })
+        })
+      })
+    })
+  }),
+  ketqua: PropTypes.shape({
+    cod: PropTypes.string
+  })
+}
 export default ItemStateUv
